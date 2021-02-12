ff748ae22f2d1df0aea0ac20b583bc5ff580e81c
Ludo Vlieger <lvlieger@twilio.com>
2021-02-12 09:21:03 -0500

    Adding notification

--- src/ReflexPlugin.js
+++ src/ReflexPlugin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VERSION, Actions, TaskHelper } from '@twilio/flex-ui';
+import { VERSION, Actions, Notifications, NotificationType } from '@twilio/flex-ui';
 
 import { FlexPlugin } from 'flex-plugin';
 
@@ -23,11 +23,25 @@ export default class ReflexPlugin extends FlexPlugin {
   init(flex, manager) {
     this.registerReducers(manager);
 
-    const options = { sortOrder: -1 };
-    flex.AgentDesktopView
-      .Panel1
-      .Content
-      .add(<CustomTaskListContainer key="ReflexPlugin-component" />, options);
+    // From SAMPLE
+    // const options = { sortOrder: -1 };
+    // flex.AgentDesktopView
+    //   .Panel1
+    //   .Content
+    //   .add(<CustomTaskListContainer key="ReflexPlugin-component" />, options);
+
+    const TRANSFER_BLOCKED = 'TransferBlocked';
+    manager.strings[TRANSFER_BLOCKED] = (
+      'This is a not-staffed test queue. Transfer to this queue has been blocked.'
+    );
+
+    Notifications.registerNotification({
+      id: TRANSFER_BLOCKED,
+      closeButton: true,
+      content: TRANSFER_BLOCKED,
+      type: NotificationType.warning,
+      timeout: 5000
+    });	
 
 
     //BLOCK TRANSFER TO SOME QUEUES
@@ -50,20 +64,6 @@ export default class ReflexPlugin extends FlexPlugin {
     });
 
 
-
-    //Queues should get loaded once in Flex
-    //Move this code to -> manager.events.addListener('pluginsLoaded', () ={  });
-    //or include in other init code
-    const getQueues = () => new Promise(async (resolve) => {
-      const query = await manager.insightsClient.instantQuery('tr-queue');
-      query.on('searchResult', (items) => {
-        resolve(items);
-      });
-      query.search('');
-    });
-
-
-    
     Actions.addListener('beforeTransferTask', async (payload, abortFunction) => {
       console.log('beforeTransferTaskPayload: ', payload);
       const targetSid = payload.targetSid;
@@ -75,8 +75,9 @@ export default class ReflexPlugin extends FlexPlugin {
         console.log('targetQueueName: ', targetQueue);
         //Either queue is new (no recent tasks) or name matches a not staffed queue
         if (!targetQueue || targetQueue.queue_name.toLowerCase().includes('not')) {
-          //Modify alert pop up message
-          alert('No Agents Available.  Transfer canceled.');
+          //Replace alert with Flex Notification
+          //alert('No Agents Available.  Transfer canceled.');
+          Notifications.showNotification(TRANSFER_BLOCKED);
           abortFunction();
         }
       }
@@ -84,24 +85,47 @@ export default class ReflexPlugin extends FlexPlugin {
 
 
     //PLAY AUDIO ON INBOUND CALLS
-    const audio = new Audio('https://api.twilio.com/cowbell.mp3');
-    const playAudio = reservation => {
-      audio.play();
-      ['accepted', 'canceled', 'rejected', 'rescinded', 'timeout'].forEach(e => {
-        reservation.on(e, () => audio.pause());
-      });
-    };
+    // const audio = new Audio('https://api.twilio.com/cowbell.mp3');
+    // const playAudio = reservation => {
+    //   audio.play();
+    //   ['accepted', 'canceled', 'rejected', 'rescinded', 'timeout'].forEach(e => {
+    //     reservation.on(e, () => audio.pause());
+    //   });
+    // };
 
-    manager.workerClient.on('reservationCreated', reservation => {
-      console.log('reservationCreated: ', reservation);
-      const isVoiceQueue = reservation.task.taskChannelUniqueName === 'voice';
-      const isInboundTask = reservation.task.attributes.direction === 'inbound';
-      if (isVoiceQueue && isInboundTask) {
-        console.log('Task is incoming call');
-        playAudio(reservation);
+    // manager.workerClient.on('reservationCreated', reservation => {
+    //   console.log('reservationCreated: ', reservation);
+    //   const isVoiceQueue = reservation.task.taskChannelUniqueName === 'voice';
+    //   const isInboundTask = reservation.task.attributes.direction === 'inbound';
+    //   if (isVoiceQueue && isInboundTask) {
+    //     console.log('Task is incoming call');
+    //     playAudio(reservation);
+    //   }
+    // });
+
+
+    //UTILS?
+
+
+    const addCallDataToTask = async (task, callSid, conferenceSid) => {
+      const { attributes } = task;
+      const { conversations } = attributes;
+      const newAttributes = { ...attributes };
+
+      let newConv = {};
+      if (conversations) {
+        newConv = { ...conversations };
       }
-    });
+      //add outcome
+      newConv.outcome = 'Completed';
+      newAttributes.conversations = newConv;
 
+      newAttributes.call_sid = callSid;
+      if (conferenceSid) {
+        newAttributes.conference = { sid: conferenceSid };
+      }
+      await task.setAttributes(newAttributes);
+    }
 
 
 
